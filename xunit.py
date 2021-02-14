d8a57fcea51a2a8cc7ae6be3267d58fd67c94841
NITA-san <ysnitta@gmail.com>
2021-02-14 23:46:53 +0900

    Add assert method

--- xunit.py
+++ xunit.py
@@ -1,19 +1,28 @@
 class TestCase:
     def __init__(self,name):
         self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
 
 class WasRun(TestCase):
     def __init__(self,name):
         self.wasRun = None
         super().__init__(name)
-    def run(self):
-        method = getattr(self, self.name)
-        method()
     def testMethod(self):
         self.wasRun = 1
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
+
+# test = WasRun("testMethod")
+# print(test.wasRun)
+# test.run()
+# print(test.wasRun)
 
