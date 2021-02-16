6857b5f8a2f3d4b5c5400eb9504572dd7a44bb3c
NITA-san <ysnitta@gmail.com>
2021-02-16 19:01:27 +0900

    Add testBrokenMethod

--- xunit.py
+++ xunit.py
@@ -4,11 +4,13 @@ class TestCase:
     def setUp(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
-        return TestResult()
+        return result
     def tearDown(self):
         pass
 
@@ -19,6 +21,8 @@ class WasRun(TestCase):
         self.log = self.log + "testMethod "
     def tearDown(self):
         self.log = self.log + "tearDown"
+    def testBrokenMethod(self):
+        raise Exception
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
@@ -29,10 +33,19 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         result = test.run()
         assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 class TestResult:
+    def __init__(self):
+        self.runConunt=0
+    def testStarted(self):
+        self.runConunt = self.runConunt + 1
     def summary(self):
-        return "1 run, 0 failed"
+        return "%d run, 0 failed" % self.runConunt
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
+#TestCaseTest("testFailedResult").run()
