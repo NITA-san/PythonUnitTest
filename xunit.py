9ecf54e3400da46ff35780059392fd12a7ee67df
NITA-san <ysnitta@gmail.com>
2021-02-16 18:28:57 +0900

    add Failed Test

--- xunit.py
+++ xunit.py
@@ -10,17 +10,14 @@ class TestCase:
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
         self.log = "setUp "
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod "
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-    def testSetUp(self):
-        self.test.run()
-        assert("setUp testMethod " == self.test.log)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown" == test.log)
 
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
